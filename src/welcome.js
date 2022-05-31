8756bc0facce56871acf9a016b8af3300dbb8cd7
LPHung <phuhung16820@gmail.com>
2022-05-31 08:04:45 +0700

    fix welcome

--- src/welcome.js
+++ src/welcome.js
@@ -1,12 +1,12 @@
 module.exports = (client) =>
 {
     const channelId = '980991505151975445'
-    const targetChannel= '941555019264716844'
+    const targetChannelId= '941555019264716844'
     client.on('guildMemberAdd', (member) =>
     {
         const message = `Chào yangho <@${member.id}> tới với Mì Arena nhóe! Hãy vào kênh ${member.guild.channels.cache
-            .get(targetChannel)
-            .toString()}`
+            .get(targetChannelId)
+            .toString()} để xưng danh`
         const channel = member.guild.channels.cache.get(channelId)
         channel.send(message)
     })
