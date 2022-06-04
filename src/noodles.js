b3f5d0cd70b2dc4e98efb441d7d404da5a806541
LPHung <phuhung16820@gmail.com>
2022-06-05 02:41:38 +0700

    music function (play and queue)

--- src/noodles.js
+++ src/noodles.js
@@ -1,59 +1,21 @@
 require("dotenv").config();
 
 const { Client, Intents, MessageEmbed } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 const WOKCommands = require('wokcommands')
 const path = require('path')
 const welcome = require('./welcome')
 const command = require('./command')
 const config = require('./config.json')
 const memberCount = require('./member-count')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('@discord-api-types/v9')
+const fs = require('fs')
+const { Player } = require('discord-player')
 
-client.on('ready', () =>
-{
-    new WOKCommands(client, {
-        commandsDir: path.join(__dirname, 'commands'),
-        showWarns: false,
-        mongoUri: process.env.mongoPath
-    })
+const CLIENT_ID = "943142960772579338"
+const GUILD_ID="941555019264716841"
 
-    console.log(`${client.user.tag} đang bay tới đây. Víuuuuu`)
-
-    welcome(client)
-
-    command(client, 'help', (message) =>
-    {
-        message.channel.send(`
-            Nhìn xuống bàn phím đi, gõ mấy cái này nè:
-            *help - Show help menu
-        `)
-    })
-
-    command(client, 'in4', (message) =>
-    {
-        const exampleEmbed = new MessageEmbed()
-            .setColor('#4752C4')
-            .setTitle('Welcome to Livestream của Egg Mì')
-            .setURL('https://www.nimo.tv/user/684138458')
-            .setAuthor({ name: '[Egg] Mì', iconURL: 'https://img.nimo.tv/t/1599518054884/202203171647542428136_1599518054884_avatar.png/w240_l0/img.webp', url: 'https://www.nimo.tv/user/684138458' })
-            .setDescription('Chú hề ở mọi vũ trụ')
-            .setThumbnail('https://img.nimo.tv/t/1629511985499/202205211653168769119_1629511985499_avatar.png/w180_l0/img.webp')
-            .addFields(
-                { name: 'Nimo ID:', value: '684138458' },
-                { name: '\u200B', value: '\u200B' },
-                { name: 'FaceBook', value: 'https://www.facebook.com/miiduyennn', inline: true },
-                { name: 'Instagram', value: 'https://www.instagram.com/migerme/', inline: true },
-                { name: 'Tiktok', value: 'https://www.tiktok.com/@migerme', inline: true },
-            )
-            .setImage('https://img.nimo.tv/t/1599518054884/202203171647542428136_1599518054884_avatar.png/w240_l0/img.webp')
-            .setTimestamp()
-            .setFooter({ text: 'Nimo Show', iconURL: 'https://img.nimo.tv/t/1599518054884/202203171647542428136_1599518054884_avatar.png/w240_l0/img.webp' });
-
-        message.channel.send({ embeds: [exampleEmbed] });
-    })
-
-    memberCount(client)
-})
 
 client.on('guildCreate', guild => {
     const channel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
@@ -100,4 +62,106 @@ client.on('messageCreate', (message) =>
         message.reply('Mì cute đóoooo 🐱🐱🐱')
     } 
 })
+
+const LOAD_SLASH = process.args[2] == 'load'
+client.slashcommands = new Client.Collection()
+client.player = new Player(client, {
+    ytdOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 <<25
+    }
+})
+
+let commands = []
+
+const slashFiles = fs.readdirSync('./slash').filter(file => file.endsWith('.js'))
+
+for (const file of slashFiles) {
+    const slashcmd = require(`./slash/${file}`)
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+    if (LOAD_SLASH)
+        commands.push(slashcmd.data.toJSON())
+}
+
+if (LOAD_SLASH) {
+    const rest = new REST({ version: "9" }).setToken(process.env.DISCORDJS_BOT_TOKEN)
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+        .then(() =>
+        {
+            console.log("Successfully loaded")
+            process.exit(0)
+        })
+        .catch((error) => {
+            if (err) {
+                console.log(err)
+                process.exit(1)
+        }
+        })
+}
+
+else {
+
+    client.on('ready', () => {
+    new WOKCommands(client, {
+        commandsDir: path.join(__dirname, 'commands'),
+        showWarns: false,
+        mongoUri: process.env.mongoPath
+    })
+
+    console.log(`${client.user.tag} đang bay tới đây. Víuuuuu`)
+
+    welcome(client)
+
+    command(client, 'help', (message) =>
+    {
+        message.channel.send(`
+            Nhìn xuống bàn phím đi, gõ mấy cái này nè:
+            *help - Show help menu
+        `)
+    })
+
+    command(client, 'in4', (message) =>
+    {
+        const exampleEmbed = new MessageEmbed()
+            .setColor('#4752C4')
+            .setTitle('Welcome to Livestream của Egg Mì')
+            .setURL('https://www.nimo.tv/user/684138458')
+            .setAuthor({ name: '[Egg] Mì', iconURL: 'https://img.nimo.tv/t/1599518054884/202203171647542428136_1599518054884_avatar.png/w240_l0/img.webp', url: 'https://www.nimo.tv/user/684138458' })
+            .setDescription('Chú hề ở mọi vũ trụ')
+            .setThumbnail('https://img.nimo.tv/t/1629511985499/202205211653168769119_1629511985499_avatar.png/w180_l0/img.webp')
+            .addFields(
+                { name: 'Nimo ID:', value: '684138458' },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'FaceBook', value: 'https://www.facebook.com/miiduyennn', inline: true },
+                { name: 'Instagram', value: 'https://www.instagram.com/migerme/', inline: true },
+                { name: 'Tiktok', value: 'https://www.tiktok.com/@migerme', inline: true },
+            )
+            .setImage('https://img.nimo.tv/t/1599518054884/202203171647542428136_1599518054884_avatar.png/w240_l0/img.webp')
+            .setTimestamp()
+            .setFooter({ text: 'Nimo Show', iconURL: 'https://img.nimo.tv/t/1599518054884/202203171647542428136_1599518054884_avatar.png/w240_l0/img.webp' });
+
+        message.channel.send({ embeds: [exampleEmbed] });
+    })
+
+    memberCount(client)
+    })
+
+    client.on("interaction", (interaction) =>
+    {
+        async function handleCommand()
+        {
+            if (!interaction.isCommand())
+                return;
+            const slashcmd = client.slashcommands.get(interaction.commandName)
+
+            if (!slashcmd) interaction.reply("Not a valid slash command")
+            
+            await interaction.deferReply()
+
+            await slashcmd.run({ client, interaction })
+        }
+        handleCommand()
+    })
+}
+
 client.login(process.env.DISCORDJS_BOT_TOKEN);
